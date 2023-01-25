7d0d845f92520107cbbc009540858cb2429aee46
TW-JZT <john.tian@trustwave.com>
2023-01-24 21:37:58 -0500

    adding universal values

---
+++ src/util/config.py
@@ -0,0 +1,8 @@
+QUIET = 20
+MODERATE = 40
+LOUD = 60
+BASE_KEYWORDS = ['an unknown topic', 'something unimportant', 'nothing of note']
+KEYWORDS = ["tf", "townsfolk", "outsider", "minion", "demon", "alignment"
+    "poison", "poisoned", "drunk", "incorrect info",
+    "day", "night", "town", "town square",
+    "kill", "killed", "chose", "choose"]
