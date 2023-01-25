8077d42fa23ff62e87e3083da9c17017794a6b8a
TW-JZT <john.tian@trustwave.com>
2023-01-24 21:37:29 -0500

    update schema for public info

--- src/schemas/message.py
+++ src/schemas/message.py
@@ -9,14 +9,17 @@ class Message(Base):
 
     id = Column(Integer, primary_key=True)
     content = Column(String)
+    publicInfo = Column(String)
     day = Column(Integer)
     nomination = Column(Boolean)
     toPlayerName = Column(String)
     fromPlayerID = Column(Integer, ForeignKey('player.id'))
     fromPlayer = relationship("Player", back_populates="messages")
 
-    def __init__(self, fromPlayer: Player, toPlayerName, content: str, day: int, nomination=False):
+    def __init__(self, fromPlayer: Player, toPlayerName, content: str, 
+    publicInfo: str, day: int, nomination=False):
         self.content = content
+        self.publicInfo = publicInfo
         self.day = day
         self.fromPlayer = fromPlayer
         self.toPlayerName = toPlayerName
